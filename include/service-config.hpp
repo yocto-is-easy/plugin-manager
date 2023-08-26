a4b4f29f007f49789c1858f551e1e5bca8c382ae
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-08-26 10:06:51 +0300

    Create directory for config if there is no
    Signed-off-by: Mykhailo Didur <mikhaildidur2003@gmail.com>

--- include/service-config.hpp
+++ include/service-config.hpp
@@ -1,15 +1,22 @@
 #pragma once
 
 #include <string>
+#include <filesystem>
 
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+
+namespace fs = std::filesystem;
 
 // class that loads and parses the config file
 class ServiceConfig {
 public:
     ServiceConfig(const std::string& config_file_path = "/etc/plugin-manager/config.json") {
-        boost::property_tree::read_json(config_file_path, config);
+        if (fs::exists(config_file_path)) {
+            boost::property_tree::read_json(config_file_path, config);    
+        } else {
+            fs::create_directories(fs::path(config_file_path).parent_path());
+        }
     }
 
     std::string get_remote_url() {
