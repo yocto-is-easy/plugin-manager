7f82beab110c0cf3983a8a47f602519dc79324b6
Mykhailo Didur <mikhaildidur2003@gmail.com>
2023-10-01 12:34:47 +0300

    Add stubs for the unimplemented functions

--- include/service-impl.hpp
+++ include/service-impl.hpp
@@ -12,6 +12,7 @@ using namespace std;
 
 INIT_MIDF_SERVER(plugin_manager);
 
+// interface for managing plugins
 MIDF_IMPL_FUNC(std::vector<std::string>, plugin_manager, get_plugins_list) () {
     return std::vector<std::string>({"plugin1", "plugin2", "plugin3"});
 }
@@ -42,6 +43,30 @@ MIDF_IMPL_FUNC(bool, plugin_manager, is_installed, std::string) (std::string plu
 
 MIDF_IMPL_FUNC(bool, plugin_manager, is_enabled, std::string) (std::string plugin_name) {
     return true;
+}
+
+// inteface for the plugins' initialization
+MIDF_IMPL_FUNC(bool, plugin_manager, register_plugin, std::string) (std::string plugin_name) {
+    return true;
+}
+
+MIDF_IMPL_FUNC(bool, plugin_manager, register_function, std::string, std::string, midf::function<std::string, std::string>, std::string)
+(std::string plugin_name, std::string function_name, midf::function<std::string, std::string> func, std::string func_description/*json*/) {
+    return true;
+}
+
+// interface to interract with the plugins
+MIDF_IMPL_FUNC(std::string, plugin_manager, get_plugin_info, std::string) (std::string plugin_name) {
+    return "";
+}
+
+MIDF_IMPL_FUNC(std::string, plugin_manager, get_function_info, std::string, std::string)
+(std::string plugin_name, std::string function_name) {
+    return "";
+}
+
+MIDF_IMPL_FUNC(PluginFunction, plugin_manager, get_function, std::string, std::string) (std::string plugin_name, std::string function_name) {
+    return midf::function<std::string, std::string>(1337, "temp_service", "temp_func");
 }
 
 void start_plugin_manager_async() {
